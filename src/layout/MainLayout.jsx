85e788696e33343749e9c791d5a54fdd46681396
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-06-22 00:21:57 +0600

    grocary collage  project

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='w-11/12 mx-auto mt-4'>
        <Outlet/>
     </div>
   )
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ProductsDashboard from './ProductsDashboard'
+import Customer from '../components/Customer'
 
 const Home = () => {
   return (
     <div>
+      <h1 className='text-2xl font-bold text-center my-4'>Welcome to the Bill Payments App</h1>
+      <Customer/>
       <ProductsDashboard/>
     </div>
   )
